915d1913516ac43d7561e1cf33e1c0f79fd67612
DEV-LIEDSON <rfliedson007@gmail.com>
2025-07-02 10:43:18 -0300

    restart repository

--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:leads/customer/leads_view.dart';
-
-void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: LeadsView()));
-}
